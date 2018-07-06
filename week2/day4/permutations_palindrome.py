988916eca3da6225516993fcbfb16a5a7f6f4b47
karthik-reddy96 <40566279+karthik-reddy96@users.noreply.github.com>
2018-07-06 11:35:59 +0530

    Update permutations_palindrome.py

--- week2/day4/permutations_palindrome.py
+++ week2/day4/permutations_palindrome.py
@@ -1,7 +1,7 @@
 import unittest
 
 from itertools import permutations
-
+"""
 def has_palindrome_permutation(the_string):
 
     
@@ -14,7 +14,23 @@ def has_palindrome_permutation(the_string):
     
     
     return False
+   """ 
+def has_palindrome_permutation(string):
+
+    # Check if any permutation of the input is a palindrome
+    dictionary = {}
+    for i in string:
+        if i in dictionary:
+            dictionary[i]+=1
+        else:
+            dictionary[i]=1
     
+    count = 0
+    for i in dictionary:
+        if dictionary[i]%2==1:
+            count+=1
+
+    return count==1 or count==0
 # Tests
 
 class Test(unittest.TestCase):
