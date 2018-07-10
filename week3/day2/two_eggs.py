175425658d70945b509f8c75b5e7d644a3b6e0b9
Karthik-reddy96 <gskarthikreddy96@gmail.com>
2018-07-10 16:37:41 +0530

    add

---
+++ week3/day2/two_eggs.py
@@ -0,0 +1,17 @@
+need = {}
+def needed(egg, floor):
+    if (egg, floor) in need:
+        return needed[(egg, floor)]
+    if egg == 1:
+        return floor
+    if egg > 1:
+        low = floor
+        for f in range(floor):
+            resultIfEggBreaks = need(egg - 1, f)
+            resultIfeggurvives = need(egg, floor - (f + 1))
+            result = max(resultIfEggBreaks, resultIfeggurvives)
+            if result < low:
+                low = result
+        need[(egg, floor)] = 1 + low
+        return 1 + low
+print (needed(2, 100))
